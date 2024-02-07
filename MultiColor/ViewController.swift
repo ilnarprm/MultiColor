057ba2b8a37248c938774279a4abc389c99890a8
ilnarprm <ilnar_prm@icloud.com>
2024-02-07 22:03:28 +0500

    work version

--- MultiColor/ViewController.swift
+++ MultiColor/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    @IBOutlet var multiColorView: UIView!
+
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        multiColorView.layer.cornerRadius = 15
+
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
     }
 
+    private func setColor() {
+        multiColorView.backgroundColor = UIColor(
+            red: CGFloat(
+                redSlider.value
+            ),
+            green: CGFloat(
+                greenSlider.value
+            ),
+            blue: CGFloat(
+                blueSlider.value
+            ),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        setColor()
+    }
+
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = string(from: greenSlider)
+        setColor()
+    }
+
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = string(from: blueSlider)
+        setColor()
+    }
 }
-
